2595011244bb22ba5401c5bad07a2cb4a1a5ede6
govinda-kamath <govinda.kamath@gmail.com>
2020-06-06 20:16:34 -0700

    fixed rand reads

--- pipeline_wrapper.py
+++ pipeline_wrapper.py
@@ -1,6 +1,7 @@
 import os
 
 cmd_0 = "python makeFolders.py"
+print(cmd_0)
 os.system(cmd_0)
 
 datasetLst = [line.rstrip('\n') for line in open('NCTC_ds.txt')]
@@ -13,14 +14,19 @@ for i in range(len(datasetLst)):
     print(cmd_1)
     os.system(cmd_1)
     
+    cmd_12 = "python generateRandReads.py --dataset "+bact+"_filtered --num_jobs 50"
+    print(cmd_12)
+    os.system(cmd_12)
+
+
     cmd_2 = "python runSVDexperiments_pipelined.py --dataset "+bact+"_filtered --num_jobs 50 --num_rand 5"
     print(cmd_2)
-    os.system(cmd_2)
+    #os.system(cmd_2)
     
 cmd_3 = "python generateAUCs_pipelined.py --datasets NCTC_ds.txt --num_jobs 50 --ground_truth daligner --theta .3"
 print(cmd_3)
-os.system(cmd_3)
+#os.system(cmd_3)
 
 cmd_4 = "python generateColProb_pipelined.py --datasets NCTC_ds.txt --num_jobs 50"
 print(cmd_4)
-os.system(cmd_4)
+#os.system(cmd_4)
